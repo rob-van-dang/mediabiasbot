740e87c9bdc8b1ff2d2a37453eb4dd5dd52c1580
Dalton Navalta <dantun11@gmail.com>
2019-10-06 14:51:51 -0700

    Use submissiont stream

--- main.py
+++ main.py
@@ -3,11 +3,27 @@ import logging
 import praw
 
 logging.basicConfig()
-logging.root.setLevel(logging.NOTSET)
-log = logging.getLogger('mediabiasbot')
+LOG = logging.getLogger('mediabiasbot')
+LOG.setLevel(logging.INFO)
+SUBREDDIT_NAME = 'testingground4bots'
+
+
+def check_comments(submission):
+    submission.comment_sort = 'new'
+    for tl_comment in submission.comments:
+        if tl_comment.body:
+            LOG.info(tl_comment.body)
+
 
 # MAIN ########################################################################
 if __name__ == "__main__":
     reddit = praw.Reddit()
     if not reddit.read_only:
-        log.info("Logged in Successfully")
+        LOG.info("======= Logged in Successfully =======")
+
+    sub = reddit.subreddit(SUBREDDIT_NAME)
+    for submission in sub.stream.submissions():
+        LOG.info("===" + submission.id + "===")
+        LOG.info("===" + submission.title + "===")
+        LOG.info("===" + submission.url + "===")
+        check_comments(submission)
