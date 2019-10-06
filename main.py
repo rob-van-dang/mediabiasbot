92205957e3d951404697a52ea528a52ce76ef4ae
Dalton Navalta <dantun11@gmail.com>
2019-10-06 15:07:35 -0700

    Check streams alternately

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ LOG.setLevel(logging.INFO)
 SUBREDDIT_NAME = 'testingground4bots'
 
 
-def check_comments(submission):
-    submission.comment_sort = 'new'
-    for tl_comment in submission.comments:
-        if tl_comment.body:
-            LOG.info(tl_comment.body)
+def check_comment(comment):
+    LOG.info(comment.body)
+
+
+def check_submission(submission):
+    LOG.info("===" + submission.id + "===")
+    LOG.info("===" + submission.title + "===")
+    LOG.info("===" + submission.url + "===")
 
 
 # MAIN ########################################################################
@@ -20,10 +23,19 @@ if __name__ == "__main__":
     reddit = praw.Reddit()
     if not reddit.read_only:
         LOG.info("======= Logged in Successfully =======")
-
     sub = reddit.subreddit(SUBREDDIT_NAME)
-    for submission in sub.stream.submissions():
-        LOG.info("===" + submission.id + "===")
-        LOG.info("===" + submission.title + "===")
-        LOG.info("===" + submission.url + "===")
-        check_comments(submission)
+
+    submission_stream = sub.stream.submissions(pause_after=-1)
+    comment_stream = sub.stream.comments(pause_after=-1)
+    while True:
+        # Check submission stream
+        for submission in submission_stream:
+            if submission is None:
+                break
+            check_submission(submission)
+
+        # Check comment stream
+        for comment in comment_stream:
+            if comment is None:
+                break
+            check_comment(comment)
