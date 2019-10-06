83c5e5c013b4b950117f369670d93d377cb1e3b5
Dalton Navalta <dantun11@gmail.com>
2019-10-06 13:49:14 -0700

    Fix logging

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import logging
+
 import praw
+
+logging.basicConfig()
+logging.root.setLevel(logging.NOTSET)
+log = logging.getLogger('mediabiasbot')
 
 # MAIN ########################################################################
 if __name__ == "__main__":
     reddit = praw.Reddit()
-    print(reddit.read_only)
+    if not reddit.read_only:
+        log.info("Logged in Successfully")
